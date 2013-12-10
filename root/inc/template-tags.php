262e428fd97b7cfba99383828c7e8bec61b8c272
Bronson Quick <bronson@sennza.com.au>
2013-12-10 11:36:56 +1000

    Split navigation template tag into context specific tags

--- root/inc/template-tags.php
+++ root/inc/template-tags.php
@@ -2,58 +2,68 @@
 /**
  * Custom template tags for this theme.
  *
- * Eventually, some of the functionality here could be replaced by core features
+ * Eventually, some of the functionality here could be replaced by core features.
  *
- * @package {%= title %}
+ * @package {%= prefix %}
  */
 
-if ( ! function_exists( '{%= prefix %}_content_nav' ) ) :
+if ( ! function_exists( '{%= prefix %}_paging_nav' ) ) :
 /**
- * Display navigation to next/previous pages when applicable
+ * Display navigation to next/previous set of posts when applicable.
+ *
+ * @return void
  */
-function {%= prefix %}_content_nav( $nav_id ) {
-	global $wp_query, $post;
-
-	// Don't print empty markup on single pages if there's nowhere to navigate.
-	if ( is_single() ) {
-		$previous = ( is_attachment() ) ? get_post( $post->post_parent ) : get_adjacent_post( false, '', true );
-		$next = get_adjacent_post( false, '', false );
-
-		if ( ! $next && ! $previous )
-			return;
-	}
-
-	// Don't print empty markup in archives if there's only one page.
-	if ( $wp_query->max_num_pages < 2 && ( is_home() || is_archive() || is_search() ) )
+function {%= prefix %}_paging_nav() {
+	// Don't print empty markup if there's only one page.
+	if ( $GLOBALS['wp_query']->max_num_pages < 2 ) {
 		return;
-
-	$nav_class = ( is_single() ) ? 'post-navigation' : 'paging-navigation';
-
+	}
 	?>
-	<nav role="navigation" id="<?php echo esc_attr( $nav_id ); ?>" class="<?php echo $nav_class; ?>">
-		<h1 class="screen-reader-text"><?php _e( 'Post navigation', '{%= language %}' ); ?></h1>
+	<nav class="navigation paging-navigation" role="navigation">
+		<h1 class="screen-reader-text"><?php _e( 'Posts navigation', '{%= language %}' ); ?></h1>
+		<div class="nav-links">
 
-	<?php if ( is_single() ) : // navigation links for single posts ?>
+			<?php if ( get_next_posts_link() ) : ?>
+			<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', '{%= language %}' ) ); ?></div>
+			<?php endif; ?>
 
-		<?php previous_post_link( '<div class="nav-previous">%link</div>', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', '{%= language %}' ) . '</span> %title' ); ?>
-		<?php next_post_link( '<div class="nav-next">%link</div>', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', '{%= language %}' ) . '</span>' ); ?>
+			<?php if ( get_previous_posts_link() ) : ?>
+			<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', '{%= language %}' ) ); ?></div>
+			<?php endif; ?>
 
-	<?php elseif ( $wp_query->max_num_pages > 1 && ( is_home() || is_archive() || is_search() ) ) : // navigation links for home, archive, and search pages ?>
-
-		<?php if ( get_next_posts_link() ) : ?>
-		<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', '{%= language %}' ) ); ?></div>
-		<?php endif; ?>
-
-		<?php if ( get_previous_posts_link() ) : ?>
-		<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', '{%= language %}' ) ); ?></div>
-		<?php endif; ?>
-
-	<?php endif; ?>
-
-	</nav><!-- #<?php echo esc_html( $nav_id ); ?> -->
+		</div><!-- .nav-links -->
+	</nav><!-- .navigation -->
 	<?php
 }
-endif; // {%= prefix %}_content_nav
+endif; // {%= prefix %}_paging_nav
+
+if ( ! function_exists( '{%= prefix %}_post_nav' ) ) :
+/**
+ * Display navigation to next/previous post when applicable.
+ *
+ * @return void
+ */
+function {%= prefix %}_post_nav() {
+	// Don't print empty markup if there's nowhere to navigate.
+	$previous = ( is_attachment() ) ? get_post( get_post()->post_parent ) : get_adjacent_post( false, '', true );
+	$next     = get_adjacent_post( false, '', false );
+
+	if ( ! $next && ! $previous ) {
+		return;
+	}
+	?>
+	<nav class="navigation post-navigation" role="navigation">
+		<h1 class="screen-reader-text"><?php _e( 'Post navigation', '{%= language %}' ); ?></h1>
+		<div class="nav-links">
+
+			<?php previous_post_link( '%link', _x( '<span class="meta-nav">&larr;</span> %title', 'Previous post link', '{%= language %}' ) ); ?>
+			<?php next_post_link(     '%link', _x( '%title <span class="meta-nav">&rarr;</span>', 'Next post link',     '{%= language %}' ) ); ?>
+
+		</div><!-- .nav-links -->
+	</nav><!-- .navigation -->
+	<?php
+}
+endif; // {%= prefix %}_post_nav
 
 if ( ! function_exists( '{%= prefix %}_comment' ) ) :
 /**
--- root/single.php
+++ root/single.php
@@ -14,7 +14,7 @@ get_header(); ?>
 
 			<?php get_template_part( 'content', 'single' ); ?>
 
-			<?php {%= prefix %}_content_nav( 'nav-below' ); ?>
+			<?php {%= prefix %}_post_nav(); ?>
 
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template
