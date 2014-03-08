eb7db3f78569f4f7e9a199ab5b3382639c50b9d9
Bronson Quick <bronson@sennza.com.au>
2014-03-08 20:26:42 +1000

    Remove .com support. Fixes #7

--- root/footer.php
+++ root/footer.php
@@ -12,7 +12,6 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<?php do_action( '{%= prefix %}_credits' ); ?>
 			<a href="http://wordpress.org/" rel="generator"><?php printf( __( 'Proudly powered by %s', '{%= language %}' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
 			<?php printf( __( 'Theme: %1$s by %2$s.', '{%= language %}' ), '{%= language %}', '<a href="http://www.sennza.com.au/" rel="designer">Sennza</a>' ); ?>
